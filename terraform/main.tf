c00a9750e5901a7095a6fd319ca7605184d7bc6b
Dev26x <dev26x@outlook.com>
2024-08-16 11:18:06 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,50 +1,74 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = "sockshop"
-  cidr = "10.123.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "socks-shop-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/hakeem-shop" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/hakeem-shop" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "19.15.3"
 
-  cluster_name                   = "sockshop"
-  cluster_version                = "1.30"
+  cluster_name    = "hakeem-shop"
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type                    = "AL2_x86_64"
+    associate_public_ip_address = true
   }
 
   eks_managed_node_groups = {
-    hakeem_node_1 = {
-      min_size     = 1
-      max_size     = 4
-      desired_size = 1
+    one = {
+      name = "node-group-1"
 
       instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
-    hakeem_node_2 = {
-      min_size     = 1
-      max_size     = 4
-      desired_size = 1
+
+    two = {
+      name = "node-group-2"
 
       instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
-
 }
-
